230486ca697e6a6d50c6092ef5c47ecf31fb7c29
Edward Cheung <30495078+edwardcheungc@users.noreply.github.com>
2017-10-09 14:55:41 +0800

    test

--- app/src/main/java/com/cft/testss/MainActivity2.java
+++ app/src/main/java/com/cft/testss/MainActivity2.java
@@ -5,11 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
 //test 1234
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
